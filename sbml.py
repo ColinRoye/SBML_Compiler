43bf970cd437ffc620d3c257783783783b33aa42
ColinRoye <croye97@gmail.com>
2019-10-27 19:27:08 -0400

    expression evaluator nearly finished

---
+++ sbml.py
@@ -0,0 +1,277 @@
+import sys
+import ply.lex as lex
+import ply.yacc as yacc
+
+# mypl.py
+# Tokenizer, Parser, Evaluator for a simple propositional logic
+
+# List of token names
+
+
+tokens = (
+          'INTEGER', 'FLOAT', 'BOOL', 'STRING', #DATA TYPES
+          'R_PAREN', 'L_PAREN',
+          'R_BRACK', 'L_BRACK',
+          'COMMA',
+         # 'F_SLASH',
+          'MULT',
+          'POW',
+          'DIV',
+          'MOD',
+          'PLUS',
+          'MINUS',
+          'IN',
+          'CONS',
+          'COLON',
+          'NOT',
+          'AND_ALSO',
+          'OR_ELSE',
+          'LTE',
+          'GTE',
+          'EQ_EQ',
+          'NEQ',
+          'LT',
+          'GT',
+          'EQ',
+          'BOOLEAN',
+          'HASHTAG'
+          )
+t_HASHTAG = r'\#'
+t_L_PAREN = r'\('
+t_R_PAREN = r'\)'
+t_L_BRACK = r'\['
+t_R_BRACK = r']'
+t_COMMA = r','
+t_POW = r'\*\*'
+t_MULT= r'\*'
+t_DIV = r'div'
+t_MOD = r'mod'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_IN = r'in'
+# t_CONS = r'::'
+t_COLON = r':'
+t_NOT = r'not'
+t_AND_ALSO = r'andalso'
+t_OR_ELSE = r'orelse'
+t_LTE = r'<='
+t_GTE = r'>='
+t_EQ_EQ = r'=='
+t_NEQ = r'<>'
+t_LT = r'<'
+t_GT = r'>'
+t_EQ   = r'='
+t_BOOLEAN = r'True|False'
+
+def t_INTEGER(t):
+    r'(-?)(\d+)'
+    t.value = NumNode(t.value)
+    return t
+def t_FLOAT(t):
+    r'(-?)(((\d)*\.(\d+)((e(-?)\d+)?)))'
+    t.value = NumNode(t.value)
+    return t
+
+def t_STRING(t):
+     r'(\"[^\"]*\")|(\'[^\']*\')'
+     t.value = Node(t.value[1:-1])
+     return t
+
+# def t_VARIABLE(t):
+#     r'[a-z]\d{0,2}'
+#     t.value = t.value
+#     return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print("SYNTAX ERROR: %s at %d" % (t.value[0], t.lexer.lineno))
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+def reduce(expr):
+    if expr != None and hasattr(expr, 'type'):
+        while(hasattr(expr, 'type')):
+            expr = expr.eval()
+        return expr
+    else:
+        return None
+
+
+
+
+
+class BinOpNode():
+    def __init__(self, e1, op, e2):
+        self.e1 = e1
+        self.e2 = e2
+        self.op = op
+    def eval(self):
+        e1 = reduce(self.e1)
+        e2 = reduce(self.e2)
+        if self.e1.type == 'num' and self.e1.type == 'num':
+            if self.op == '**':
+                return e1 ** e2
+            if self.op == '*':
+                return e1 * e2
+            if self.op == '/':
+                return e1 / e2
+            if self.op == '%':
+                return e1 % e2
+            if self.op == '+':
+                return e1 + e2
+            if self.op == '-':
+                return e1 - e2
+        if self.e1.type == 'string' and self.e1.type == 'string':
+            if self.op == '+':
+                return e1 + e2
+class BooleanOpNode():
+    def __init__(self, e1, op, e2):
+        self.e1 = e1
+        self.e2 = e2
+        self.op = op
+    def eval(self):
+        def eval(self):
+            e1 = reduce(self.e1)
+            e2 = reduce(self.e2)
+
+            if self.op == '==':
+                return e1 == e2
+            if self.op == '!=':
+                return e1 != e2
+            if self.op == 'andalso':
+                return e1 and e2
+            if self.op == 'orelse':
+                return e1 or e2
+            if self.op == '<=':
+                return e1 <= e2
+            if self.op == '>=':
+                return e1 >= e2
+            if self.op == '>':
+                return e1 > e2
+            if self.op == '<':
+                return e1 < e2
+
+
+class NumNode():
+    def __init__(self, val):
+        self.val = val
+        self.type = "num"
+    def eval(self):
+        if '.' in self.val:
+            return float(self.val)
+        else:
+            return int(self.val)
+
+def p_expr(t):
+    """expr : INTEGER
+            | FLOAT
+            | STRING
+            | list
+            | tuple"""
+    t[0] = t[1]
+
+def p_binop_expr(t):
+    """expr : expr POW expr
+            | expr MULT expr
+            | expr DIV expr
+            | expr MOD expr
+            | expr PLUS expr
+            | expr MINUS expr"""
+    t[0] = BinOpNode(t[1],t[2],t[3]).eval()
+
+
+def p_boolop(t):
+    """expr : expr AND_ALSO expr
+            | expr OR_ELSE expr
+            | expr LTE expr
+            | expr GTE expr
+            | expr EQ_EQ expr
+            | expr NEQ expr
+            | expr LT expr
+            | expr GT expr"""
+    t[0] = BooleanOpNode(t[1],t[2],t[3])
+def p_listop(t):
+    """expr : expr IN expr
+            | expr CONS expr"""
+    t[0] = ListOpNode(t[1],t[2],t[3])
+
+def p_negationOp(t):
+    """expr : NOT expr"""
+    t[0] = NegationOpNode(t[1],t[2])
+
+
+
+def p_sequence(t):
+       """sequence : expr COMMA sequence
+                   | expr"""
+       if len(t) == 4:
+           t[0] =  [t[1]] + t[3]
+       else:
+           t[0] = [t[1]]
+
+def p_list(t):
+    """list : L_BRACK sequence R_BRACK
+            | L_BRACK R_BRACK"""
+    if len(t) == 3:
+        t[0] = []
+    else:
+        t[0] = t[2]
+
+def p_list_index(t):
+    """expr : list L_BRACK expr R_BRACK"""
+    t[0] = IndNode(t[1], t[3])
+
+def p_tuple(t):
+    """tuple : L_PAREN sequence R_PAREN
+             | L_PAREN R_PAREN"""
+    if len(t) == 3:
+        t[0] = ()
+    else:
+        t[0] = tuple(t[2])
+
+def p_tuple_index(t):
+    """expr : HASHTAG INTEGER tuple"""
+    t[0] = IndNode(t[3], t[2])
+
+#
+# def p_parenthesized(t):
+#     """parenthesized : L_PAREN expr R_PAREN"""
+#
+
+
+
+
+
+
+
+
+
+
+def p_error(t):
+    print("SYNTAX ERROR")
+
+
+
+# Manually setting precedence and associativity to resolve ambiguity in the
+# grammar.
+precedence = (
+
+              ('left', 'L_BRACK'),
+              ('left', 'L_PAREN')
+              )
+
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = input("Enter a proposition: ")
+    except EOFError:
+        break
+    if not s:
+        continue
+    result = parser.parse(s, debug = True)
+    print("RESULT:", result)
